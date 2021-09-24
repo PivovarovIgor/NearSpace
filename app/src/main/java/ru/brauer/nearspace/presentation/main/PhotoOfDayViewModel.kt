6c8f8bcc722a2f44afae02a778bc3ea84348306c
PivIgor <brauer.igor@gmail.com>
2021-09-24 23:17:02 +0300

    Deleted unnecessary code.

--- app/src/main/java/ru/brauer/nearspace/presentation/main/PhotoOfDayViewModel.kt
+++ app/src/main/java/ru/brauer/nearspace/presentation/main/PhotoOfDayViewModel.kt
@@ -1,6 +1,5 @@
 package ru.brauer.nearspace.presentation.main
 
-import android.util.Log
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,17 +7,13 @@ import ru.brauer.nearspace.data.repository.RepositoryImpl
 import ru.brauer.nearspace.domain.entities.Apod
 import ru.brauer.nearspace.domain.interactor.ApodInteractor
 import ru.brauer.nearspace.domain.interactor.ApodInteractorImpl
-import kotlin.random.Random
 
 class PhotoOfDayViewModel : ViewModel() {
 
     private val interactor: ApodInteractor = ApodInteractorImpl(RepositoryImpl())
     private val cachedData: MutableMap<Long?, MutableLiveData<PhotoOfDayAppState>> = mutableMapOf()
 
-    private val id = Random.nextInt(1000)
-
     fun observe(lifecycleOwner: LifecycleOwner, renderData: RenderData, date: Long?) {
-        Log.i("PhotoOfDayViewModel", id.toString())
         if (!cachedData.containsKey(date)) {
             cachedData += date to MutableLiveData<PhotoOfDayAppState>()
         }
@@ -28,8 +23,7 @@ class PhotoOfDayViewModel : ViewModel() {
         }
     }
 
-    fun getPhotoOfDay(date: Long?) {
-        cachedData[date]?.value = PhotoOfDayAppState.Loading
+    fun getPhotoOfDay(date: Long?) { cachedData[date]?.value = PhotoOfDayAppState.Loading
         interactor.getApod(date, object : ApodInteractor.CallbackApod {
             override fun notify(apod: Apod?, onServerFailureMessage: String?, ex: Throwable?) {
                 cachedData[date]?.value = apod?.let { PhotoOfDayAppState.Success(it) }
