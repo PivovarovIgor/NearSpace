6dc898e5a4eb6e7601f89a485a448bd0f935d187
PivIgor <brauer.igor@gmail.com>
2021-10-04 15:15:42 +0300

    Deleting with animation on RecyclerView.

--- app/src/main/java/ru/brauer/nearspace/presentation/notes/NotesAdapter.kt
+++ app/src/main/java/ru/brauer/nearspace/presentation/notes/NotesAdapter.kt
@@ -1,13 +1,20 @@
 package ru.brauer.nearspace.presentation.notes
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import ru.brauer.nearspace.databinding.RecyclerViewItemOfNotesBinding
 import ru.brauer.nearspace.domain.entities.Note
 
-class NotesAdapter(private val viewModel: NotesViewModel) :
+class NotesAdapter(
+    private val viewModel: NotesViewModel,
+    private val registerForContextMenu: (view: View) -> Unit
+) :
     RecyclerView.Adapter<NotesAdapter.ViewHolder>() {
+
+    var contextMenuPosition: Int = RecyclerView.NO_POSITION
+    private set
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = RecyclerViewItemOfNotesBinding.inflate(
@@ -15,7 +22,14 @@ class NotesAdapter(private val viewModel: NotesViewModel) :
             parent,
             false
         )
-        return ViewHolder(binding)
+        registerForContextMenu(binding.root)
+        val vh = ViewHolder(binding)
+        binding.root.setOnLongClickListener {
+            contextMenuPosition = vh.adapterPosition
+            it.showContextMenu()
+            true
+        }
+        return vh
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -29,5 +43,6 @@ class NotesAdapter(private val viewModel: NotesViewModel) :
         fun bind(note: Note) {
             binding.noteText.text = note.noteText
         }
+
     }
 }
--- app/src/main/java/ru/brauer/nearspace/presentation/notes/NotesFragment.kt
+++ app/src/main/java/ru/brauer/nearspace/presentation/notes/NotesFragment.kt
@@ -1,17 +1,24 @@
 package ru.brauer.nearspace.presentation.notes
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ru.brauer.nearspace.R
 import ru.brauer.nearspace.databinding.FragmentNotesBinding
 
 class NotesFragment : Fragment() {
 
     private val viewModel: NotesViewModel by activityViewModels()
     private var binding: FragmentNotesBinding? = null
+    private val adapter: NotesAdapter by lazy {
+        NotesAdapter(viewModel) { v ->
+            registerForContextMenu(v)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,7 +31,39 @@ class NotesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding?.apply {
-            listOfNotes.adapter = NotesAdapter(viewModel)
+            listOfNotes.adapter = adapter
+            listOfNotes.addItemDecoration(
+                DividerItemDecoration(
+                    view.context,
+                    LinearLayoutManager.VERTICAL
+                )
+            )
+        }
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu,
+        v: View,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        val menuInflater: MenuInflater = requireActivity().menuInflater
+        menuInflater.inflate(R.menu.context_menu_item, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.context_menu_item_delete -> {
+                binding?.apply {
+                    val contextMenuPosition = adapter.contextMenuPosition
+                    if (contextMenuPosition != RecyclerView.NO_POSITION) {
+                        viewModel.notes.removeAt(contextMenuPosition)
+                        adapter.notifyItemRemoved(contextMenuPosition)
+                    }
+                }
+                true
+            }
+            else -> false
         }
     }
 
