0150c31389b02f68c49ede69b62f7efbe31a2e01
PivIgor <brauer.igor@gmail.com>
2021-10-09 20:56:05 +0300

    Optimizing in code of animation by selecting in RecyclerView.

--- app/src/main/java/ru/brauer/nearspace/presentation/notes/NotesAdapter.kt
+++ app/src/main/java/ru/brauer/nearspace/presentation/notes/NotesAdapter.kt
@@ -1,9 +1,11 @@
 package ru.brauer.nearspace.presentation.notes
 
+import android.animation.Animator
 import android.animation.ArgbEvaluator
 import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
@@ -23,6 +25,7 @@ class NotesAdapter(
     companion object {
         private const val MOVING_UP = -1
         private const val MOVING_DOWN = 1
+        private const val DURATION_ANIMATION_ITEM = 5000L
     }
 
     var contextMenuPosition: Int = RecyclerView.NO_POSITION
@@ -35,15 +38,10 @@ class NotesAdapter(
             parent,
             false
         )
-        registerForContextMenu(binding.root)
+        registerForContextMenu(binding.noteText)
 
         val vh = ViewHolder(binding)
         binding.run {
-            root.setOnLongClickListener {
-                contextMenuPosition = vh.adapterPosition
-                it.showContextMenu()
-                true
-            }
 
             buttonNoteUp.setOnClickListener {
                 moveItem(MOVING_UP) { vh.adapterPosition }
@@ -53,6 +51,11 @@ class NotesAdapter(
                 moveItem(MOVING_DOWN) { vh.adapterPosition }
             }
 
+            noteText.setOnLongClickListener {
+                contextMenuPosition = vh.adapterPosition
+                it.showContextMenu()
+                true
+            }
             noteText.setOnClickListener {
                 viewModel.notes[vh.adapterPosition] =
                     (viewModel.notes[vh.adapterPosition].first to
@@ -111,8 +114,18 @@ class NotesAdapter(
 
     inner class ViewHolder(private val binding: RecyclerViewItemOfNotesBinding) :
         RecyclerView.ViewHolder(binding.root), ItemTouchHelperViewHolder {
+
+        private var animator: Animator? = null
+
         fun bind(noteRec: Pair<Note, Boolean>) {
             binding.run {
+                animator?.let {
+                    if (it.isStarted) {
+                        it.cancel()
+                    }
+                    animator = null
+                }
+                itemView.setBackgroundColor(Color.WHITE)
                 noteText.text = noteRec.first.noteText
                 noteDescription.text = noteRec.first.description
                 noteDescription.visibility = if (noteRec.second) View.VISIBLE else View.GONE
@@ -120,21 +133,28 @@ class NotesAdapter(
         }
 
         override fun onItemSelected() {
-            ValueAnimator.ofObject(ArgbEvaluator(), Color.WHITE, Color.TRANSPARENT).apply {
-                duration = 200L
-                addUpdateListener {
-                    itemView.setBackgroundColor(it.animatedValue as Int)
-                }
-            }.start()
+            animate(Color.WHITE, Color.TRANSPARENT)
         }
 
         override fun onItemClear() {
-            ValueAnimator.ofObject(ArgbEvaluator(), Color.TRANSPARENT, Color.WHITE).apply {
-                duration = 200L
+            animate(Color.TRANSPARENT, Color.WHITE)
+        }
+
+        private fun animate(beginValue: Int, endValue: Int) {
+            animator?.let {
+                if (it.isStarted) {
+                    it.pause()
+                }
+            }
+            val startValue = itemView.background.let {
+                (it as? ColorDrawable)?.color ?: beginValue
+            }
+            animator = ValueAnimator.ofObject(ArgbEvaluator(), startValue, endValue).apply {
+                duration = DURATION_ANIMATION_ITEM
                 addUpdateListener {
                     itemView.setBackgroundColor(it.animatedValue as Int)
                 }
-            }.start()
+            }.also { it.start() }
         }
     }
 }
